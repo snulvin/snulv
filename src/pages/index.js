4689c17a33cf48fcf54bf72936497c53ee7501b6
Snorre Ulvin Garberg <snorre.ulvin@gmail.com>
2020-02-04 15:25:18 +0100

    Added several links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.css';
+import '../style/main.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+function IndexPage() {
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+  return (
+    <div className="m-4">
+      <h1 className="text-danger">Hi people</h1>
+      <p>Welcome to Snu.lv, let me serve you some links tho</p>
+      <div className="d-flex flex-row">
+        <div className="border p-2 w-25">
+          <h4 className="text-danger">News tho</h4>
+          <ul>
+            <li><a className="d-block" href="https://www.nrk.no">NRK</a></li>
+            <li><a className="d-block" href="https://news.ycombinator.com">Hacker News</a></li>
+          </ul>
+        </div>
+        <div className="border p-2 w-25">
+          <h4 className="text-danger">DNDBeyond yo</h4>
+          <ul>
+            <li><a className="d-block" href="https://www.dndbeyond.com/">Main</a></li>
+            <li><a className="d-block" href="https://www.dndbeyond.com/campaigns/620013">Frozen</a></li>
+            <ul>
+              <li><a className="d-block" href="https://www.dndbeyond.com/profile/Qulvin/characters/16457360">Pip</a></li>
+              <li><a className="d-block" href="https://www.dndbeyond.com/profile/derputmeister/characters/16455296">Dareth</a></li>
+              <li><a className="d-block" href="https://www.dndbeyond.com/profile/celis/characters/16443828">Dekle</a></li>
+              <li><a className="d-block" href="https://www.dndbeyond.com/profile/5andshark/characters/17313363">Levias</a></li>
+              <li><a className="d-block" href="https://www.dndbeyond.com/profile/SIFRI/characters/14190418">Ursula</a></li>
+            </ul>
+          </ul>
+        </div>
+        <div className="border p-2 w-25">
+          <h4 className="text-danger">Bored bro</h4>
+          <ul>
+            <li><a className="d-block" href="https://www.reddit.com/">Roddat</a></li>
+            <li><a className="d-block" href="https://www.youtube.com">YahTabb</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  );
+}
 
 export default IndexPage
